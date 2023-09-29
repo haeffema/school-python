5ab1ebb04a6c00c4897a003f27fd8fa13b8c31a1
Haeffele, Maximilian <haeffema@gmail.com>
2023-09-29 10:30:57 +0200

    moved path out of function

--- functions/utils/search_func/list_generator.py
+++ functions/utils/search_func/list_generator.py
@@ -1,10 +1,15 @@
 import os
 import random
 
+ROOT_DIR = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+PATH = f'{ROOT_DIR}\\functions\\ressources\\number_lists'
+
 
 def generate_list(filename: str, amount_of_random_numbers: int):
-    ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-    int_list = [int(i) for i in open(f'{ROOT_DIR}\\functions\\ressources\\number_lists\\{filename}').readlines()]
+    ROOT_DIR = os.path.dirname(os.path.dirname(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+    int_list = [int(i) for i in open(f'{PATH}\\{filename}').readlines()]
     amount = int_list.pop(0)
     int_list.sort()
     return amount, int_list, [random.choice(int_list) for _ in range(amount_of_random_numbers)]
